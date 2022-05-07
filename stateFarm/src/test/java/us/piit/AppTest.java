942919c6e883e85d6a820b65b508b5295710617c
faridgraiche <farid.graiche150@gmail.com>
2022-05-07 17:51:29 -0400

    jnaf

--- stateFarm/src/test/java/us/piit/AppTest.java
+++ stateFarm/src/test/java/us/piit/AppTest.java
@@ -2,12 +2,9 @@ package us.piit;
 
 
 import base.CommonAPI;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.util.concurrent.TimeUnit;
+
 
 public class AppTest extends CommonAPI {
 
@@ -16,12 +13,8 @@ public void test1() throws InterruptedException {
     click("//button[@class=\"-oneX-header-top-menu-btn \"and@data-for='findanagent']");
     type("//input[@id=\"oneX-findAnAgentZipCode\"]","11214");
     click("//button[@id=\"findAnAgentButton\"]");
-    click("//a[@class=\"-oneX-link--block visit-agent-site\"and @title='Sammy Martinez']");
 
-    switchToDifferentWindow();
-    selectFromDropDown("//select[@id=\"quote-product-select\"]","Homeowners");
-    type("//input[@id=\"quote-zip-field\"]","11214");
-    click("//button[@id=\"get-quote-button\"]");
+
 
 
 }
